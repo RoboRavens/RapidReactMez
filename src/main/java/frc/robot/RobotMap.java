8734ca97fb46d9abbb29f98f275083acf83ad1de
milligansarah <98560997+milligansarah@users.noreply.github.com>
2023-12-13 21:18:31 -0500

    Drivetrain Offsets

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -16,24 +16,24 @@ public final class RobotMap {
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 6;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 24;
     // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(73.301); // practice
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(140); // competition
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(40); // competition
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22;
     // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(320.625); // practice
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(168); // competition
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(158); // competition
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 21;
     // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(191.338); // practice
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(237); // competition
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(150); // competition
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 4;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23;
     // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(201.885); // practice
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(274); // competition
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(200); // competition
 
 }
